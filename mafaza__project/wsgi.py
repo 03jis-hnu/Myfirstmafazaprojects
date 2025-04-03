404998b218cf9a932ccbd6ee49475544d93d7437
03jis-hnu <03jishtest@gmail.com>
2025-04-03 16:18:35 +0530

    Update wsgi.py

--- mafaza__project/wsgi.py
+++ mafaza__project/wsgi.py
@@ -1,11 +1,6 @@
 import os
-import sys
-
-# Ensure the project directory is in the system path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mafaza__project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mafaza__project.settings")
 
 application = get_wsgi_application()
